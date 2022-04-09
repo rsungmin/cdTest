1e16102f953ba11d5b879b6e2ba9a7fbacb48c41
rsungmin <s14526231@gmail.com>
2022-04-09 22:55:51 +0900

    모의고사

--- cdTest/src/cdTest/Solution15.java
+++ cdTest/src/cdTest/Solution15.java
@@ -66,6 +66,7 @@ public class Solution15 {
         ArrayList<Integer> list = new ArrayList<>();
         if(max==cnt1) {list.add(1);}
         if(max==cnt2) {list.add(2);}
+        
         if(max==cnt3) {list.add(3);}
         
         answer = new int[list.size()];
@@ -75,9 +76,39 @@ public class Solution15 {
         return answer;
     }
 	
+	
+	public int[] solution2(int[] answers) {
+		int[] answer= {};
+		int[] one = {1,2,3,4,5};
+		int[] two = {2,1,2,3,2,4,2,5};
+		int[] three = {3,3,1,1,2,2,4,4,5,5};
+		int cnt1 = 0;
+		int cnt2 = 0;
+		int cnt3 = 0;
+		
+		for(int i=0;i<answers.length;i++) {
+			if(one[i%one.length]==answers[i]) {cnt1++;}
+			if(two[i%two.length]==answers[i]) {cnt2++;}
+			if(three[i%three.length]==answers[i]) {cnt3++;}
+		}
+		
+		 int max = Math.max(Math.max(cnt1, cnt2),cnt3); 
+	     ArrayList<Integer> list = new ArrayList<>();
+	     if(max==cnt1) {list.add(1);}
+	     if(max==cnt2) {list.add(2);}
+	     if(max==cnt3) {list.add(3);}
+	     
+	     answer = new int[list.size()];
+	        for(int i=0; i<answer.length; i++) {
+	        	answer[i]=list.get(i);
+	        }
+	     
+		return answer;
+	}
 	public static void main(String[] args) {
 		Solution15 sol = new Solution15();
-		int[] answers= {1,2,3,4,5};
-		System.out.println(Arrays.toString(sol.solution(answers)));
+		//int[] answers= {1,2,3,4,5};
+		int[] answers= {1,3,2,4,2};
+		System.out.println(Arrays.toString(sol.solution2(answers)));
 	}
 }
