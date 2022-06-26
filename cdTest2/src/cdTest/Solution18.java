3a0d5c7e43cbd9dc8f62c5cfd822f6c78ec34bbf
rsungmin <s14526231@gmail.com>
2022-06-26 18:08:43 +0900

    solution18

---
+++ cdTest2/src/cdTest/Solution18.java
@@ -0,0 +1,22 @@
+package cdTest;
+
+import java.util.Scanner;
+
+//프로그래ㅓ스 - 연습문제 - 직사각형 별찍기
+//https://programmers.co.kr/learn/courses/30/lessons/12969
+
+public class Solution18 {
+	 public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        int a = sc.nextInt();
+	        int b = sc.nextInt();
+	        
+	        for(int i=0; i<b; i++) {
+	        	for(int j=0; j<a;j++) {
+	        		System.out.print("*");
+	        	}
+	        	System.out.println();
+	        }
+
+	    }
+}
